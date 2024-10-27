f422add501c90732a25815cce85ec561540c0ecc
rumesh <rumeshk066@gmail.com>
2024-10-27 20:04:21 +0530

    Inline css

--- react-test-01/src/App.jsx
+++ react-test-01/src/App.jsx
@@ -2,12 +2,15 @@
 import './App.css'
 function App() {
 
-
+ const styles_d = {
+  color:'red'
+ }
 
   return (
     <>
       <div id="wrapper">
         <p className="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem dolor ab ex nemo commodi qui vitae, laudantium corrupti? Et non, deserunt deleniti ex quibusdam sed odio accusantium neque modi!</p>
+        <p style={styles_d}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem dolor ab ex nemo commodi qui vitae, laudantium corrupti? Et non, deserunt deleniti ex quibusdam sed odio accusantium neque modi!</p>
 
       </div>
     </>
