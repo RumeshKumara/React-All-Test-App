f1df6905f979df455e60d2bf6b8bc8a1c8c2cd65
rumesh <rumeshk066@gmail.com>
2024-10-27 19:53:21 +0530

    css adding how to import

--- react-test-01/src/App.jsx
+++ react-test-01/src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import './App.css'
 function App() {
 
 
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
       <div id="wrapper">
-        
+        <p className="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem dolor ab ex nemo commodi qui vitae, laudantium corrupti? Et non, deserunt deleniti ex quibusdam sed odio accusantium neque modi!</p>
+
       </div>
     </>
   )
